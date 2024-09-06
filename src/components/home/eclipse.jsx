60a5db25f7cc3d6992eafc06cf70608c90ec28ff
emdevelopa <gyimahsunday@gmail.com>
2024-09-06 22:36:39 +0100

    changes-againg

--- src/components/home/eclipse.jsx
+++ src/components/home/eclipse.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Shape = () => {
   return (
     <div className="w-[30em] h-[14em] mb-16 mx-auto">
-      <div className="shape">xxx</div>
+      <div className="shape"></div>
     </div>
   );
 };
