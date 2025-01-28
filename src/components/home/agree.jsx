f776995a163a06eeb74acfb95e6ba8f04369a1b1
3th-Enjay <enjayjerey@gmail.com>
2025-01-28 22:27:43 +0100

    agree-updated

--- src/components/home/agree.jsx
+++ src/components/home/agree.jsx
@@ -1,11 +1,29 @@
 "use client";
-
+import Link from "next/link";
 import React, { useState } from "react";
 import Image from "next/image";
 import { FaLongArrowAltRight } from "react-icons/fa";
 import Section from "./macbookScroll"; // Section component with animations
 import ConnectButtoncomponent from "../connect";
 import Shape from "./eclipse";
+import Footer from "../footer";
+// import "./stimport { TextFade } from "./textFade";
+import FadeInSection from "../fadeInSection";
+// const printAgreement = (agreement) => {
+//   const printContent = `
+//     <h1>${agreement.title}</h1>
+//     <p>Second Party Address: ${agreement.secondPartyAddress}</p>
+//     <p>Created by  : ${agreement.creatorName}</p>
+//     <p>${agreement.content}</p>
+//   `;
+//   const printWindow = window.open('', '', 'width=800,height=600');
+//   printWindow.document.write(printContent);
+//   printWindow.document.close();
+//   printWindow.print();
+// };
+
+
+
 
 const Agree = () => {
   const [showLaunchDapps, setShowLaunchDapps] = useState(false);
@@ -13,6 +31,10 @@ const Agree = () => {
   const toggleLaunchDapps = () => {
     setShowLaunchDapps(!showLaunchDapps);
   };
+  const closeModal = () => {
+    setShowLaunchDapps(false);
+  };
+
 
   return (
     <main className="mt-20 space-y-32">
@@ -23,6 +45,16 @@ const Agree = () => {
             Create new legal agreements by providing the agreement content, the
             address of the second party, and details about the first party.
           </p>
+           {/* <TextFade
+            direction="up"
+            className="pt-0 pb-5 flex-col flex justify-center items-center space-y-0"
+          >
+            <p className="text-[1em] mb-4">
+              Create new legal agreements by providing the agreement content,
+              the <br />
+              address of the second party, and details about the first party.
+            </p>
+          </TextFade> */}
         </div>
         <div className="flex justify-center items-center">
           <div className="w-full max-w-4xl p-4">
@@ -48,6 +80,23 @@ const Agree = () => {
                 Launch Dapps
                 <FaLongArrowAltRight className="ml-1" />
               </div>
+                {/* <button className="relative max-w-sm br text-white p-3 shadow-lg transform hover:scale-105 transition-transform duration-300 border-gradient bg-opacity-50 mb-5 backdrop-filter backdrop-blur-lg flex items-center justify-center overflow-clip">
+            <span className="flex items-center">Launch Dapp</span>
+            <svg
+              className="ml-2 w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 7l5 5m0 0l-5 5m5-5H6"
+              ></path>
+            </svg>
+          </button> */}
             </a>
           </div>
         </div>
@@ -73,6 +122,23 @@ const Agree = () => {
                 trust and accountability among parties.
               </p>
             </div>
+               {/* <button className="relative max-w-sm br text-white p-3 shadow-lg transform hover:scale-105 transition-transform duration-300 border-gradient bg-opacity-50 mb-5 backdrop-filter backdrop-blur-lg flex items-center justify-center overflow-clip">
+            <span className="flex items-center">Launch Dapp</span>
+            <svg
+              className="ml-2 w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 7l5 5m0 0l-5 5m5-5H6"
+              ></path>
+            </svg>
+          </button> */}
           </div>
         </div>
       </Section>
