bd5ee955932f4030821f67adfabfcdfd15400d4c
ebubechi-ihediwa <ebubechiihediwa@gmail.com>
2025-02-22 17:22:07 +0100

    Resolved build error

--- src/app/custos-safe/file-storage/page.jsx
+++ src/app/custos-safe/file-storage/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-// import SideBar from '@/app/vidandimage_storage/components/SideBar';
-// import Header from '@/app/vidandimage_storage/components/Header';
-// import UploadOrRecord from '@/app/vidandimage_storage/components/UploadOrRecord';
 import BackNavigation from '../components/BackNavigation';
+import UploadOrReload from '../components/UploadOrRecord';
+import Header from '../components/Header';
+import SideBar from '../components/SideBar';
 
 const Main = () => {
   return (
@@ -16,15 +16,13 @@ const Main = () => {
       }}
     >
       {/* Sidebar */}
-      <SideBar />
 
       {/* Main Content */}
       <div className="flex flex-col w-full lg:w-3/4 h-auto lg:h-screen bg-opacity-70 text-white px-4 sm:px-6 lg:px-8 py-4 lg:py-0">
-        <Header />
         <div className="mt-4">
           <BackNavigation />
           <div className="mt-6">
-            <UploadOrRecord />
+            <UploadOrReload />
           </div>
         </div>
       </div>
